3a4d8f44f5cdf2a07949d012f7b91e138b2eeb79
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-03-13 16:07:37 +0100

    added option to bypass the restriction
    the developer might want to push anyway and ignore the warning, this would be helpful

--- script.sh
+++ script.sh
@@ -8,22 +8,36 @@ function git() {
         if [ $# -eq 1 ]; then
             echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You have to pass other arguments with the git push command.\033[0m"
         else
-            # Get the last argument passed to git command
-            last_arg="${!#}"
+            if [[ "$*" == *"--allow-push"* ]]; then
+                # Get the command line arguments and remove the '--allow-push' option if it's present
+                args=("$@")
+                for (( i=0; i<"${#args[@]}"; i++ )); do
+                if [[ "${args[i]}" == "--allow-push" ]]; then
+                    unset 'args[i]'
+                    break
+                fi
+                done
 
-            # Check if the last argument is either "main" or "master"
-            if [[ "$last_arg" == "main" || "$last_arg" == "master" ]]; then
-                echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $last_arg branch.\033[0m"
+                # Run the modified git command
+                /usr/bin/git "${args[@]}"
             else
-                if [[ "$last_arg" == "origin" ]]; then
-                    current_branch=$(git rev-parse --abbrev-ref HEAD)
-                    if [[ "$current_branch" == "main" || "$current_branch" == "master" ]]; then
-                        echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $current_branch branch.\033[0m"
+                # Get the last argument passed to git command
+                last_arg="${!#}"
+
+                # Check if the last argument is either "main" or "master"
+                if [[ "$last_arg" == "main" || "$last_arg" == "master" ]]; then
+                    echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $last_arg branch.\033[0m"
+                else
+                    if [[ "$last_arg" == "origin" ]]; then
+                        current_branch=$(git rev-parse --abbrev-ref HEAD)
+                        if [[ "$current_branch" == "main" || "$current_branch" == "master" ]]; then
+                            echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $current_branch branch.\033[0m"
+                        else
+                            /usr/bin/git "$@"
+                        fi
                     else
                         /usr/bin/git "$@"
                     fi
-                else
-                    /usr/bin/git "$@"
                 fi
             fi
         fi
