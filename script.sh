4395340fc1e5037760c78b20840b0b6464f715ff
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-03-14 18:51:31 +0100

    modified the error message in the script

--- script.sh
+++ script.sh
@@ -26,12 +26,12 @@ function git() {
 
                 # Check if the last argument is either "main" or "master"
                 if [[ "$last_arg" == "main" || "$last_arg" == "master" ]]; then
-                    echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $last_arg branch.\033[0m"
+                    echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $last_arg branch.\033[0m\nTo bypass this restriction [unrecommended], add \"--allow-push\" to the end of your command.\nFor more informations, please visit: https://github.com/ch33kaboo/no-push-main \n\nAborting push..."
                 else
                     if [[ "$last_arg" == "origin" ]]; then
                         current_branch=$(git rev-parse --abbrev-ref HEAD)
                         if [[ "$current_branch" == "main" || "$current_branch" == "master" ]]; then
-                            echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $current_branch branch.\033[0m"
+                            echo -e "\033[48;5;17m\033[38;5;15mno-push-main\033[0m\033[38;5;15m: \033[48;5;196m\033[38;5;15mERROR\033[0m\033[38;5;15m: You cannot push to the $current_branch branch.\033[0m\nTo bypass this restriction [unrecommended], add \"--allow-push\" to the end of your command.\nFor more informations, please visit: https://github.com/ch33kaboo/no-push-main \n\nAborting push..."
                         else
                             /usr/bin/git "$@"
                         fi
